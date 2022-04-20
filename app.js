d965d35ed9a7c6b0265d39144aea1eb8daf69576
Vaibhavi1707 <vaibhavilokegaonkar@gmail.com>
2022-04-21 05:18:41 +0530

    Completed student backend

--- app.js
+++ app.js
@@ -9,6 +9,92 @@ app.use(express.json());
 app.use('/student', studentRoute);
 app.use('/admin', adminRoute);
 
+// read
+
+app.get('/', (req, res) => {
+    var params = {
+        TableName: "users",
+        Key: {
+            "email_id": "user@mail.com"
+        }
+    };
+
+    documentClient.get(params, (err, data) => {
+        if (err)
+        {
+            console.log(JSON.stringify(err, null, 2));
+        }
+        else 
+        {
+            res.send(data);
+        }
+    });
+});
+
+// write 
+app.get('/write', (req, res) => {
+    var params = {
+        TableName: "users",
+        Item: {
+            "email_id": "user@mail.com",
+            "name": "user"
+        }
+    }
+
+    // Error callback
+    documentClient.put(params, (err, data) => {
+        if (err)
+        {
+            console.log(err);
+        }
+        res.send(data);
+    });
+});
+
+// update
+app.get('/update', (req, res) => {
+    var params = {
+        TableName: "users",
+        Key: {
+            "email_id": "user@mail.com",
+        },
+        UpdateExpression: "set username = :newName",
+        ExpressionAttributeValues: {
+            ":newName": "helloworld"
+        },
+        ReturnValues: "UPDATED_NEW"
+    };
+
+    documentClient.update(params, (err, data) => {
+        if (err)
+        {
+            console.log(err);
+        }
+        res.send(data);
+    });
+});
+
+// delete
+app.get('/delete', (req, res) => {
+    var params = {
+        TableName: "users",
+        Key: {
+            "email_id": "user@mail.com",
+        }
+    };
+
+    documentClient.delete(params, (err, data) => {
+        if (err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            res.send("success");
+        }
+    });
+});
+
 app.listen(port, () => {
     console.log("Listening to " + port);
 });
